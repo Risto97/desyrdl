afd4c2abb350b9241c81e8a2c4216462ec14f0ca
Michael Büchler <michael.buechler@desy.de>
2020-12-10 14:00:20 +0100

    [WIP] templates: use multiple RDL files and templates
    VhdlFormatter.py now accepts multiple .rdl input files from which it
    creates the register model, just like the example print_hierarchy.py
    does.
    
    It now also globs for all "./templates/*.vhd.in" files, processes them
    and puts the results in the "./HECTARE/" output folder.

--- VhdlFormatter.py
+++ VhdlFormatter.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import string
+from pathlib import Path # get filenames
 
 from systemrdl import RDLCompileError, RDLCompiler, RDLWalker
 from systemrdl import RDLListener
-from systemrdl.node import RegNode, FieldNode, AddressableNode
+from systemrdl.node import RegNode, FieldNode, AddressableNode, AddrmapNode
 
 
 class VhdlFormatter(string.Formatter):
@@ -42,7 +43,7 @@ class VhdlFormatter(string.Formatter):
                 return ''.join([self.format(
                     template,
                     regtype=regtype,
-                    name=regtype.get_property("name"))
+                    name=regtype.type_name)
                     for regtype in value])
             elif what == "fields":
 
@@ -54,7 +55,7 @@ class VhdlFormatter(string.Formatter):
                     sw_access=field.get_property("sw").name,
                     hw_access=field.get_property("hw").name,
                     reset=field.get_property("reset"),
-                    name=field.get_property("name"))
+                    name=field.type_name)
                     for i,field in enumerate(value.fields())])
             else:
                 return "-- VOID" # this shouldn't happen
@@ -78,37 +79,59 @@ class RegtypeListener(RDLListener):
                 self.regtypes[node.type_name] = node
 
 
-
 def main():
-    rdlfile = sys.argv[1]
+    rdlfiles = sys.argv[1:]
 
     # Create an instance of the compiler
     rdlc = RDLCompiler()
 
     try:
-        rdlc.compile_file(rdlfile)
+        for rdlfile in rdlfiles:
+            rdlc.compile_file(rdlfile)
         root = rdlc.elaborate()
     except RDLCompileError:
         # A compilation error occurred. Exit with error code
         sys.exit(1)
-    top_node = root.get_child_by_name("top")
+    top_node = next(root.children())
 
-    # creating "views" on dictionaries: d.keys(), d.values(), d.items()
-    regtypes = dict()
-    walker = RDLWalker(unroll=True)
-    walker.walk(root, RegtypeListener(regtypes=regtypes))
+    # no need to unroll arrays since non-homogenous arrays are not supported anyways
+    walker = RDLWalker(unroll=False)
+
+    # currently we're collecting register types for each of the AddrmapNodes,
+    # not globally
+    #regtypes = dict()
+    #walker.walk(root, RegtypeListener(regtypes=regtypes))
     #print("".join(["found type of RegNode {}\n".format(x.type_name) for x in regtypes.values()]))
 
     vf = VhdlFormatter()
 
-    filename = 'templates/minimal.vhd.in'
+    out_dir = Path("HECTARE")
+    out_dir.mkdir(exist_ok=True)
 
-    with open(filename, 'r') as f_in:
-        s_in = f_in.read()
-    with open(filename.rpartition('.')[0], 'w') as f_out:
-        hdl = vf.format(s_in, regtypes=regtypes.values())
-        f_out.write(hdl)
-        print(hdl)
+    # component type name, either definitive or anonymous: systemrdl.component.Component.type_name
+    # The instantiated element is Component.inst_name, right?!
+
+    for node in root.descendants():
+        if isinstance(node, AddrmapNode):
+            # obtain a dictionary of register types
+            regtypes = dict()
+            walker.walk(node, RegtypeListener(regtypes=regtypes))
+            for tpl in Path('./templates').glob('*.vhd.in'):
+                with tpl.open('r') as f_in:
+                    s_in = f_in.read()
+                # creating "views" on dictionaries: d.keys(), d.values() or d.items()
+                # modname should be unique wthin the top addrmap so the pkg name is unique, too
+                ip_folder_path = ''.join(["modules/", node.type_name, "/hdl"]) # where the user logic lies
+                print("ip_folder_path =", ip_folder_path)
+                hdl = vf.format(s_in, modname=node.get_path_segment(), regtypes=regtypes.values())
+
+                suffix = "".join(tpl.suffixes) # get the ".vhd.in"
+                out_file = "".join([str(tpl.name).replace(suffix, ""), "_", node.inst_name, ".vhd"])
+                out_path = Path(out_dir, out_file)
+                print(out_path)
+                with out_path.open('w') as f_out:
+                    f_out.write(hdl)
+                #print(hdl)
 
 if __name__ == '__main__':
     main()
