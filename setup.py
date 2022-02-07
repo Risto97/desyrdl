35d9fb291a882bb798088e7c672aeca31695729e
Michael Büchler <michael.buechler@desy.de>
2022-02-07 19:28:26 +0100

    build: use pyproject.toml + setup.cfg
    The preferred way of defining the setup is by a static `setup.cfg`
    file [1]. The build tools are defined in a `pyproject.toml`. This is
    accordoing to PEP 517 and PEP 518.
    
    [1] https://packaging.python.org/en/latest/tutorials/packaging-projects/

--- setup.py
+++ setup.py
@@ -1,33 +1,7 @@
 #!/usr/bin/env python
-"""DesyRDL tool.
 
-setup.py installation package.
-"""
+# Keep this file for pip <21.1 and 'install --editable'
+
 from setuptools import setup
-
-setup(
-    name="DesyRDL",
-    version="0.1.2",
-    author="Michael Büchler <michael.buechler@desy.de>, Lukasz Butkowski <lukasz.butkowski@desy.de>",
-    author_email="michael.buechler@desy.de",
-
-    description="DesyRDL: Tool for address space and registers generation",
-
-    # package and requirements
-    packages=['desyrdl'],
-
-    include_package_data=True,
-
-    setup_requires=["wheel",
-                    "setuptools>=42"],
-
-    install_requires=[
-        "systemrdl-compiler >= 1.12",
-    ],
-
-    # Scripts
-    entry_points={
-        'console_scripts': ['desyrdl=desyrdl.desyrdl:main'],
-    },
-
-)
+if __name__ == "__main__":
+    setup()
