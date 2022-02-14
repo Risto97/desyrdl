Risto97/desyrdl

commit 35f990e0594d95cd154dbe554109e53ccb19f197

initial templates cleanup
for now only register type
now generating registers entities instead of lib component

desyrdl/libraries/vhdl/reg_field_storage.vhd
@@ -26,56 +26,77 @@ use desyrdl.common.all;
 
 entity reg_field_storage is
   generic (
-            -- contains an array of field info
-            g_info : t_field_info
-          );
+    -- contains an array of field info
+    g_info : t_field_info
+  );
   port (
-         pi_clock : in std_logic;
-         pi_reset : in std_logic;
-         -- to/from software
-         pi_sw_we   : in std_logic;
-         pi_sw_stb  : in std_logic;
-         pi_sw_data : in std_logic_vector(g_info.len-1 downto 0);
-         po_sw_data : out std_logic_vector(g_info.len-1 downto 0);
-         -- to/from hardware logic
-         pi_hw_we    : in std_logic;
-         pi_hw_data  : in std_logic_vector(g_info.len-1 downto 0);
-         po_hw_data  : out std_logic_vector(g_info.len-1 downto 0);
-         po_hw_swmod : out std_logic
-       );
+    pi_clock : in std_logic;
+    pi_reset : in std_logic;
+    -- to/from software
+    pi_sw_rd_stb : in std_logic;
+    pi_sw_wr_stb : in std_logic;
+    pi_sw_data   : in std_logic_vector(g_info.len-1 downto 0);
+    po_sw_data   : out std_logic_vector(g_info.len-1 downto 0);
+    -- to/from hardware logic
+    pi_hw_we    : in std_logic;
+    pi_hw_data  : in std_logic_vector(g_info.len-1 downto 0);
+    po_hw_data  : out std_logic_vector(g_info.len-1 downto 0);
+    po_hw_swmod : out std_logic;
+    po_hw_swacc : out std_logic
+  );
 end entity reg_field_storage;
 
 architecture rtl of reg_field_storage is
-  signal field_reg : std_logic_vector(g_info.len-1 downto 0);
+  signal field_reg   : std_logic_vector(g_info.len-1 downto 0);
+  signal sw_wr_stb_q : std_logic;
+  signal sw_rd_stb_q : std_logic;
 begin
 
   -- check if the hardware side (logic) has a write enable signal
   -- or has no write access to the register
-  gen_hw_we : if g_info.hw_we or g_info.hw_access = C_R or g_info.hw_access = C_NA generate
+  gen_hw_we : if g_info.we = 1 or g_info.hw = C_R or g_info.hw = C_NA generate
     prs_write : process(pi_clock)
     begin
       if rising_edge(pi_clock) then
         if pi_reset = '1' then
-          field_reg <= g_info.def_val(g_info.len-1 downto 0);
+          field_reg <= std_logic_vector(to_unsigned(g_info.defval, g_info.len));
           po_hw_swmod <= '0';
+          po_hw_swacc <= '0';
+          sw_wr_stb_q <= '0';
+          sw_rd_stb_q <= '0';
         else
           -- software write has precedence FIXME
-          if pi_sw_stb = '1' and pi_sw_we = '1' and (g_info.sw_access = C_W or g_info.sw_access = C_RW) then
-            -- TODO handle software access side effects (rcl/rset, woclr/woset, swacc/swmod)
+          -- TODO handle software access side effects (rcl/rset, woclr/woset, swacc/swmod)
+          if pi_sw_wr_stb = '1' and (g_info.sw = C_W or g_info.sw = C_RW) then
             field_reg <= pi_sw_data;
             -- software access side effects
-            po_hw_swmod <= '1';
+            sw_wr_stb_q <= '1';
             --po_hw_swmod <= '1' when g_info.swmod = True else '0';
+          elsif pi_sw_rd_stb = '1' and (g_info.sw = C_R or g_info.sw = C_RW) then
+            sw_rd_stb_q <= '1';
           -- hardware write might get lost FIXME
-          elsif pi_hw_we = '1' and (g_info.hw_access = C_W or g_info.hw_access = C_RW) then
+          elsif pi_hw_we = '1' and (g_info.hw = C_W or g_info.hw = C_RW) then
             field_reg <= pi_hw_data;
             -- software access side effects
             po_hw_swmod <= '0';
           else
             field_reg <= field_reg;
             -- software access side effects
-            po_hw_swmod <= '0';
+            sw_wr_stb_q <= '0';
+            sw_rd_stb_q <= '0';
           end if;
+          -- generate sw modified and sw accessed only for 1 clock cycle
+          if sw_wr_stb_q = '0' and pi_sw_wr_stb = '1'  and (g_info.sw = C_W or g_info.sw = C_RW) then
+            po_hw_swmod <= '1';
+            po_hw_swacc <= '1';
+          elsif sw_rd_stb_q = '0' and pi_sw_rd_stb = '1' and (g_info.sw = C_R or g_info.sw = C_RW) then
+            po_hw_swmod <= '0';
+            po_hw_swacc <= '1';
+          else
+            po_hw_swmod <= '0';
+            po_hw_swacc <= '0';
+          end if;
+
         end if;
       end if;
     end process;
@@ -84,28 +105,44 @@ begin
   -- write from logic continuously if there is no write enable and hW has write
   -- access. Software cannot write in this case.
   -- TODO handle C_W1 and C_RW1
-  gen_hw_no_we : if not g_info.hw_we and (g_info.hw_access = C_W or g_info.hw_access = C_RW) generate
+  gen_hw_no_we : if g_info.we = 0 and (g_info.hw = C_W or g_info.hw = C_RW) generate
     prs_write : process(pi_clock)
     begin
       if rising_edge(pi_clock) then
         if pi_reset = '1' then
           -- doesn't make so much sense here, does it?
-          field_reg <= g_info.def_val(g_info.len-1 downto 0);
+          --field_reg <= std_logic_vector(to_unsigned(g_info.defval, g_info.len));
+          field_reg <= (others => '0');
+          po_hw_swmod <= '0';
+          po_hw_swacc <= '0';
+          sw_wr_stb_q <= '0';
+          sw_rd_stb_q <= '0';
         else
           -- hardware writes all the time and software can only read
           field_reg <= pi_hw_data;
+          -- generate sw modified and sw accessed only for 1 clock cycle
+          if sw_wr_stb_q = '0' and pi_sw_wr_stb = '1'  and (g_info.sw = C_W or g_info.sw = C_RW) then
+            po_hw_swmod <= '1';
+            po_hw_swacc <= '1';
+          elsif sw_rd_stb_q = '0' and pi_sw_rd_stb = '1' and (g_info.sw = C_R or g_info.sw = C_RW) then
+            po_hw_swmod <= '0';
+            po_hw_swacc <= '1';
+          else
+            po_hw_swmod <= '0';
+            po_hw_swacc <= '0';
+          end if;
         end if;
       end if;
     end process;
   end generate;
 
   -- check for read access properties when assigning data outputs
-  with g_info.hw_access select po_hw_data <=
+  with g_info.hw select po_hw_data <=
     field_reg       when C_R,
     field_reg       when C_RW,
     field_reg       when C_RW1,
     (others => '0') when others;
-  with g_info.sw_access select po_sw_data <=
+  with g_info.sw select po_sw_data <=
     field_reg       when C_R,
     field_reg       when C_RW,
     field_reg       when C_RW1,

desyrdl/DesyListener.py
@@ -324,6 +324,7 @@ class DesyListener(RDLListener):
             context["we"] = 0 if fldx.get_property("we") is False else 1
             context["sw"] = fldx.get_property("sw").name
             context["hw"] = fldx.get_property("hw").name
+            context["const"] = 1 if fldx.get_property("hw").name is "na" or fldx.get_property("hw").name is "r" else 0
             context["reset"] = 0 if fldx.get_property("reset") is None else fldx.get_property("reset")
             context["decrwidth"] = fldx.get_property("decrwidth") if fldx.get_property("decrwidth") is not None else 1
             context["incrwidth"] = fldx.get_property("incrwidth") if fldx.get_property("incrwidth") is not None else 1

desyrdl/libraries/vhdl/pkg_desyrdl_common.vhd
@@ -4,6 +4,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- library desy;
+-- use desy.common_types.all;
+-- use desy.common_axi.all;
+
 package common is
 
   constant C_AXI4L_ADDR_WIDTH : natural := 32;
@@ -128,13 +132,13 @@ package common is
   ---------------------------------------------------------------------------
   -- SystemRDL-specific definitions
   --type t_field_access is (R, W, RW, NA);
-  subtype t_field_access is integer;
+  subtype t_access_type is integer;
+  constant C_NA  : integer := 1;
   constant C_RW  : integer := 2;
-  constant C_RW1 : integer := 5;
   constant C_R   : integer := 3;
   constant C_W   : integer := 4;
+  constant C_RW1 : integer := 5;
   constant C_W1  : integer := 6;
-  constant C_NA  : integer := 1;
 
   type t_field_type is (STORAGE, WIRE, COUNTER, INTERRUPT);
 
@@ -143,31 +147,56 @@ package common is
     len       : integer;
     upper     : integer;
     lower     : integer;
-    hw_we     : boolean;
-    sw_access : t_field_access;
-    hw_access : t_field_access;
-    def_val   : std_logic_vector(32-1 downto 0);
+    sw        : t_access_type;
+    hw        : t_access_type;
+    we        : integer;
     incrwidth : integer;
     decrwidth : integer;
+    defval    : integer;
   end record;
 
-  type t_field_info_arr is array (integer range 31 downto 0) of t_field_info;
-  constant C_FIELD_NONE : t_field_info := (WIRE, 0, 0, 0, false, C_NA, C_NA, (others => '0'), 0, 0);
+  type t_field_info_array is array (natural range <>) of t_field_info;
+
+  constant C_FIELD_NONE : t_field_info := (WIRE, 0, 0, 0, C_NA, C_NA, 0, 0, 0, 0);
 
   type t_reg_info is record
-    addr    : integer;
-    base    : integer;
-    fields  : t_field_info_arr;
-    N       : positive;
-    M       : positive;
+    -- index    : integer;
+    address  : integer;
+    elements : integer;
+    index    : integer;
+    fields   : t_field_info_array(31 downto 0);
+    --dim         : natural;
+    dim_n    : positive;
+    dim_m    : positive;
   end record;
-  constant C_REG_NONE : t_reg_info := (0, 0, (others => C_FIELD_NONE), 1, 1);
 
-  -- Maybe better to constrain t_reg_info_array. Vivado shows weird indices when unconstrained.
-  -- Must be the number of distinct register names, not one for each 2D/3D array element!
-  --type t_reg_info_array is array (0 to C_REGNAMES) of t_reg_info;
   type t_reg_info_array is array (natural range <>) of t_reg_info;
 
+  constant C_REG_NONE : t_reg_info := (0, 0, 0,(others => C_FIELD_NONE),1,1);
+
+  type t_mem_info is record
+    -- index      : integer;
+    address    : integer;
+    addrwidth  : integer;
+    datawidth  : integer;
+    entries    : integer;
+    sw         : t_access_type;
+  end record;
+
+  type t_mem_info_array is array (natural range <>) of t_mem_info;
+
+  constant C_MEM_NONE : t_mem_info := (0, 0, 0, 0, C_NA);
+
+  type t_ext_info is record
+    -- index      : integer;
+    address    : integer;
+    addrwidth  : integer;
+    size       : integer;
+  end record;
+
+  type t_ext_info_array is array (natural range <>) of t_ext_info;
+
+  constant C_EXT_NONE : t_ext_info := (0, 0, 0);
 
   -- We can't have VHDL-2008 at the moment but maybe at some point we will
 --  type t_field_signals_in is record
@@ -182,17 +211,17 @@ package common is
 --    swmod : std_logic;
 --  end record;
 
-  type tif_mem_in is record
-    ena  : std_logic;
-    wr   : std_logic;
-    addr : std_logic_vector(32-1 downto 0);
-    data : std_logic_vector(32-1 downto 0);
-  end record tif_mem_in;
-  type tif_mem_in_arr is array (natural range <>) of tif_mem_in;
+  -- type tif_mem_in is record
+  --   ena  : std_logic;
+  --   wr   : std_logic;
+  --   addr : std_logic_vector(32-1 downto 0);
+  --   data : std_logic_vector(32-1 downto 0);
+  -- end record tif_mem_in;
+  -- type tif_mem_in_arr is array (natural range <>) of tif_mem_in;
 
-  -- TODO make this a record as well just to stay consistent
-  subtype tif_mem_out is std_logic_vector(32-1 downto 0);
-  type tif_mem_out_arr is array (natural range <>) of tif_mem_out;
+  -- -- TODO make this a record as well just to stay consistent
+  -- subtype tif_mem_out is std_logic_vector(32-1 downto 0);
+  -- type tif_mem_out_arr is array (natural range <>) of tif_mem_out;
 
   -- interface types
   type t_if_type Is (DPM, AXI4, AXI4L, IBUS, WISHBONE, AVALON, NONE);

commit cb334731f9702d625bb935f4a772b32582e0631f

fix(vhdl): don't forget about a HW 'we' during SW reads
This fixes a bug introduced in 35f990e0594d95cd154dbe554109e53ccb19f197.

The additional if condition prevented the logic from detecting a write
operation from the hardware if the software was reading simultaneously.

As a result, mcta4u_fw_programmer, by spamming reads to the WORD_CONTROL
register, prevented the status bit from being updated and gave up with a
timeout error.

desyrdl/libraries/vhdl/reg_field_storage.vhd
@@ -71,19 +71,23 @@ begin
             -- software access side effects
             sw_wr_stb_q <= '1';
             --po_hw_swmod <= '1' when g_info.swmod = True else '0';
-          elsif pi_sw_rd_stb = '1' and (g_info.sw = C_R or g_info.sw = C_RW) then
-            sw_rd_stb_q <= '1';
           -- hardware write might get lost FIXME
           elsif pi_hw_we = '1' and (g_info.hw = C_W or g_info.hw = C_RW) then
             field_reg <= pi_hw_data;
             -- software access side effects
-            po_hw_swmod <= '0';
+            sw_wr_stb_q <= '0';
           else
             field_reg <= field_reg;
             -- software access side effects
             sw_wr_stb_q <= '0';
+          end if;
+
+          if pi_sw_rd_stb = '1' and (g_info.sw = C_R or g_info.sw = C_RW) then
+            sw_rd_stb_q <= '1';
+          else
             sw_rd_stb_q <= '0';
           end if;
+
           -- generate sw modified and sw accessed only for 1 clock cycle
           if sw_wr_stb_q = '0' and pi_sw_wr_stb = '1'  and (g_info.sw = C_W or g_info.sw = C_RW) then
             po_hw_swmod <= '1';